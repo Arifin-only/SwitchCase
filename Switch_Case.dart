a5a72dfc9851434ee4c1fa46f7f3cd6ee95b1a70
Arifin-only <arifin180104@gmail.com>
2024-10-07 20:52:36 +0800

    Create Switch_Case.dart

---
+++ Switch_Case.dart
@@ -0,0 +1,18 @@
+ void main(){
+   var nilai = 'E';
+
+   switch(nilai){
+     case 'A':
+     print('Nice Lulus dengan Baik Sekali');
+     break;
+     case 'B':
+     case 'C':
+     print('Anda lulus dengan baik');
+     break;
+    case 'D':
+     print('yuyyy belum bisa lulus');
+     break;
+     default:
+     print('ampih ja kuliah');
+   }
+ }
